99ec47ac2f15274d0fe6b7dd4bcfcaa364509b85
Baptiste Brassart <baptiste.brassart@gmail.com>
2019-12-30 14:46:22 +0100

    Exercise 3.3: Phonebook backend step3

--- index.js
+++ index.js
@@ -28,6 +28,11 @@ app.get('/api/persons', (request, response) => {
   response.json(persons);
 });
 
+app.get('/api/persons/:id', (request, response) => {
+  const requestedPerson = persons.find(person => person.id === Number(request.params.id));
+  requestedPerson ? response.json(requestedPerson) : response.status(404).end();
+});
+
 app.get('/info', (request, response) => {
   const html = `<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`;
   response.send(html);
