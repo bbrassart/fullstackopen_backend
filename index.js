024e099ac99dac72c937af3633fbccc2ceac7ea8
Baptiste Brassart <baptiste.brassart@gmail.com>
2019-12-31 10:14:26 +0100

    Exercise 3.7: Phonebook backend step7

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
+
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
+
+const morganMiddleWare = require('morgan');
+app.use(morganMiddleWare('tiny'));
 
 let persons = [
   {
